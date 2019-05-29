5f986154d66f20ad7006e95da386107679499744
Aaron Eline <aeline@umd.edu>
2019-05-28 21:36:33 -0400

    First commit

---
+++ inferrealtime.el
@@ -0,0 +1,11 @@
+
+
+(flycheck-define-checker infer-c
+  "Infer checker"
+  :command ("infer" "-a" "checkers" "--bufferoverrun" "--" "gcc" source)
+  :error-patterns
+  ((error line-start (file-name) ":" line ": error: " (message) line-end)
+   (error line-start (file-name) ":" line ":" column ": error: " (message) line-end))
+  :modes c-mode)
+(add-to-list 'flycheck-checkers 'infer-c)
+(flycheck-add-next-checker 'c/c++-gcc 'infer-c)
