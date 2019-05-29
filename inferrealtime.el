6005df706171894634bf93bd1a87ecc65c2e56f8
Aaron Eline <aeline@umd.edu>
2019-05-28 22:13:18 -0400

    Correctly parse GCC errors

--- inferrealtime.el
+++ inferrealtime.el
@@ -4,8 +4,9 @@
   "Infer checker"
   :command ("infer" "-a" "checkers" "--bufferoverrun" "--" "gcc" source)
   :error-patterns
-  ((error line-start (file-name) ":" line ": error: " (message) line-end)
-   (error line-start (file-name) ":" line ":" column ": error: " (message) line-end))
+  ((error line-start (file-name) ":" line ":" column ": error: " (message) line-end)
+   (info line-start (file-name) ":" line ": error: " (message) line-end))
   :modes c-mode)
+
 (add-to-list 'flycheck-checkers 'infer-c)
 (flycheck-add-next-checker 'c/c++-gcc 'infer-c)
